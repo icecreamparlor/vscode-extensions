420acfdfe2a9c7d626a77f342a6c07660b29b0bc
김희재 <heejae.kim@toss.im>
2023-05-30 15:08:12 +0900

    fix: 0.0.21 publish

--- converter/src/converter/impl/file-path-to-base64.converter.ts
+++ converter/src/converter/impl/file-path-to-base64.converter.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
+import * as vscode from "vscode";
 import { COMMAND } from "../../settings";
 import { Converter } from "../converter";
 
@@ -10,7 +11,9 @@ export class FilePathToBase64Converter implements Converter {
   }
   async convert(text: string): Promise<string> {
     if (text.startsWith(".")) {
-      const filePath = path.resolve(__dirname, text);
+      const workspaceFolders = vscode.workspace.workspaceFolders ?? [];
+      const workspacePath = workspaceFolders[0].uri.path;
+      const filePath = path.resolve(workspacePath, text);
       const buffer = fs.readFileSync(filePath);
 
       return buffer.toString("base64");
--- converter/src/converter/impl/file-path-to-plain-text.converter.ts
+++ converter/src/converter/impl/file-path-to-plain-text.converter.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
+import * as vscode from "vscode";
 import { COMMAND } from "../../settings";
 import { Converter } from "../converter";
 
@@ -10,7 +11,9 @@ export class FilePathToPlainTextConverter implements Converter {
   }
   async convert(text: string): Promise<string> {
     if (text.startsWith(".")) {
-      const filePath = path.resolve(__dirname, text);
+      const workspaceFolders = vscode.workspace.workspaceFolders ?? [];
+      const workspacePath = workspaceFolders[0].uri.path;
+      const filePath = path.resolve(workspacePath, text);
       const buffer = fs.readFileSync(filePath);
 
       return buffer.toString("utf-8");
@@ -23,9 +26,9 @@ export class FilePathToPlainTextConverter implements Converter {
 
       return buffer.toString("utf-8");
     }
-    const filePath = path.resolve(__dirname, text)
+    const filePath = path.resolve(__dirname, text);
     const buffer = fs.readFileSync(filePath);
-    
-    return buffer.toString('utf-8')
+
+    return buffer.toString("utf-8");
   }
 }
